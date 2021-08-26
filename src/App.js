e7a08b077e5495219a478774a73a50fd509e1d22
Anusree V A <anusreeva93@gmail.com>
2021-08-26 19:10:30 +0530

    Added code to get vaccination sessions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledButton } from "./styles.js";
 import "./styles.css";
 import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 let states = [];
 let districts = [];
+let selected_date = new Date().toLocaleDateString("en-GB").split(",")[0];
+console.log(selected_date);
+
+const SelectDate = () => {
+  const [startDate, setDate] = useState(new Date());
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => {
+        selected_date = date.toLocaleString("en-GB").split(",")[0];
+        console.log(selected_date);
+        setDate(date);
+        console.log();
+      }}
+      dateFormat="dd/MM/yyyy"
+    />
+  );
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -41,6 +61,35 @@ class App extends React.Component {
           }),
         });
         console.log(this.state.state_list);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getHospitals(district_id) {
+    console.log("Inside getDistricts");
+    let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${selected_date}`;
+    console.log(url);
+    fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // districts = data[0];
+        console.log(data);
+        // this.setState({
+        //   district_list: data.districts.map((state) => {
+        //     return {
+        //       label: state.district_name,
+        //       value: state.district_id,
+        //     };
+        //   }),
+        // });
+        // console.log(this.state.district_list);
       })
       .catch((err) => {
         console.log(err);
@@ -104,18 +153,26 @@ class App extends React.Component {
         <label>Choose a District</label>
 
         <Select
-          placeholder={this.state.selected_state.label}
+          placeholder={this.state.selected_district.label}
           onChange={(selectedOption) => {
-            this.setState({ selectedOption });
-            console.log(`District selected:`, selectedOption);
+            console.log(selectedOption);
+            this.setState({ selected_district: selectedOption.value }, () => {
+              console.log(`District selected:`, this.state.selected_district);
+            });
           }}
           options={this.state.district_list}
         />
 
+        <label>Choose a Date</label>
+
+        <SelectDate />
+
         <StyledButton
           type="submit"
           value="Submit"
-          onClick={this.handleSelect}
+          onClick={() => {
+            this.getHospitals(this.state.selected_district);
+          }}
         />
       </div>
     );
