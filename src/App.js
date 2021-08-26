2447a2e35066932763702494b14e2669f4257f84
Anusree V A <anusreeva93@gmail.com>
2021-08-26 20:07:23 +0530

    Added code to show session details in a table

--- src/App.js
+++ src/App.js
@@ -26,19 +26,49 @@ const SelectDate = () => {
   );
 };
 
+class Sessions extends React.Component {
+  render() {
+    console.log(this.props.info);
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Hospital Name</th>
+            <th>Address</th>
+            <th>Available Capacity Dose 1</th>
+            <th>Available Capacity Dose 2</th>
+            <th>Vaccine Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.info &&
+            this.props.info.map((hospital) => {
+              return (
+                <tr key={hospital.name}>
+                  <td>{hospital.name}</td>
+                  <td>{hospital.address}</td>
+                  <td>{hospital.available_capacity_dose1}</td>
+                  <td>{hospital.available_capacity_dose2}</td>
+                  <td>{hospital.vaccine}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       state_list: [],
       selected_state: "",
-      district_list: [],
       selected_district: "",
-      hospital_name: "",
-      address: "",
-      availability: "",
-      dose1: "",
-      dose2: "",
+      district_list: [],
+      sessions_list: [],
+      showComponent: false,
     };
   }
 
@@ -80,7 +110,10 @@ class App extends React.Component {
       .then((response) => response.json())
       .then((data) => {
         // districts = data[0];
-        console.log(data);
+        console.log(data.sessions);
+        this.setState({
+          sessions_list: data.sessions,
+        });
         // this.setState({
         //   district_list: data.districts.map((state) => {
         //     return {
@@ -136,44 +169,57 @@ class App extends React.Component {
     });
   }
 
+  buttonClick(e) {
+    e.preventDefault();
+    this.setState({
+      showComponent: true,
+    });
+    this.getHospitals(this.state.selected_district, () => {
+      console.log(e);
+      console.log(this.state);
+    });
+  }
+
   render() {
     return (
       <div>
-        <label>Choose a State</label>
-
-        <Select
-          placeholder={this.state.selected_state.label}
-          onChange={(selectedOption) => {
-            console.log("State Chosen ", selectedOption);
-            this.handleChange({ selected_state: selectedOption });
-          }}
-          options={this.state.state_list}
-        />
-
-        <label>Choose a District</label>
-
-        <Select
-          placeholder={this.state.selected_district.label}
-          onChange={(selectedOption) => {
-            console.log(selectedOption);
-            this.setState({ selected_district: selectedOption.value }, () => {
-              console.log(`District selected:`, this.state.selected_district);
-            });
-          }}
-          options={this.state.district_list}
-        />
-
-        <label>Choose a Date</label>
-
-        <SelectDate />
-
-        <StyledButton
+        <form>
+          <label>Choose a State</label>
+          <Select
+            placeholder={this.state.selected_state.label}
+            onChange={(selectedOption) => {
+              console.log("State Chosen ", selectedOption);
+              this.handleChange({ selected_state: selectedOption });
+            }}
+            options={this.state.state_list}
+          />
+          <label>Choose a District</label>
+          <Select
+            placeholder={this.state.selected_district.label}
+            onChange={(selectedOption) => {
+              console.log(selectedOption);
+              this.setState({ selected_district: selectedOption.value }, () => {
+                console.log(`District selected:`, this.state.selected_district);
+              });
+            }}
+            options={this.state.district_list}
+          />
+          <label>Choose a Date</label>
+          <SelectDate />
+          {/* <StyledButton
           type="submit"
           value="Submit"
           onClick={() => {
             this.getHospitals(this.state.selected_district);
-          }}
-        />
+          }
+        /> */}
+          <button onClick={this.buttonClick.bind(this)}> Submit </button>
+        </form>
+
+        {this.state.showComponent && this.state.sessions_list && (
+          <Sessions info={this.state.sessions_list}></Sessions>
+        )}
+        {/* <Sessions info={this.state.sessions}></Sessions>; */}
       </div>
     );
   }
