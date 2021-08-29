fdd3638f4e6fe0917b0ae7143292823269735bbe
Anusree V A <anusreeva93@gmail.com>
2021-08-29 22:51:41 +0530

    Refactored code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
-import { render } from "@testing-library/react";
-
-let states = [];
 
 const customStyles = {
   option: (provided, state) => ({
@@ -22,9 +19,7 @@ const customStyles = {
 
 function App() {
   const [state_list, setStateList] = React.useState([]);
-  const [selected_state, setState] = React.useState("");
   const [selected_state_id, setStateId] = React.useState("");
-  const [selected_district, setDistrict] = React.useState("");
   const [selected_district_id, setDistrictId] = React.useState("");
   const [district_list, setDistrictList] = React.useState([]);
   const [sessions_list, setSessionsList] = React.useState([]);
@@ -37,20 +32,15 @@ function App() {
 
   //Getting States
   React.useEffect(() => {
-    console.log("Inside getStates");
+    // console.log("Inside getStates");
     fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states", {
       method: "GET",
-      // mode: "no-cors",
       headers: {
         accept: "application/json",
       },
     })
       .then((response) => response.json())
       .then((data) => {
-        states = data.states.map((state) => {
-          return { state_name: state.state_name, state_id: state.state_id };
-        });
-
         setStateList(
           data.states.map((state) => {
             return { label: state.state_name, value: state.state_id };
@@ -63,50 +53,10 @@ function App() {
       });
   }, []);
 
-  //Getting State-Id
-  React.useEffect(() => {
-    console.log("Inside get StateId");
-    if (selected_state) {
-      console.log("Selected State: ", selected_state.label);
-      const state = states.filter((obj) => {
-        return obj.state_name === selected_state.label;
-      });
-      setStateId(state[0].state_id);
-    }
-  }, [selected_state]);
-
-  //Getting Sessions Details
-  React.useEffect(() => {
-    if (selected_district_id && selected_date && valid) {
-      console.log("Inside getSessions");
-      let date_string = selected_date.toLocaleString("en-GB").split(",")[0];
-      console.log(
-        "Selected District: ",
-        selected_district_id,
-        " Selected date: ",
-        date_string
-      );
-      let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${selected_district_id}&date=${date_string}`;
-      fetch(url, {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setSessionsList(data.sessions);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [selected_district_id, selected_date, valid]);
-
   //Getting Districts
   React.useEffect(() => {
     if (selected_state_id) {
-      console.log("Inside getDistricts");
+      // console.log("Inside getDistricts");
       let url = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${selected_state_id}`;
       fetch(url, {
         method: "GET",
@@ -124,7 +74,6 @@ function App() {
               };
             })
           );
-          // console.log(this.state.district_list);
         })
         .catch((err) => {
           console.log(err);
@@ -132,34 +81,58 @@ function App() {
     }
   }, [selected_state_id]);
 
-  // If any input changes, table should be reset
-  useEffect(() => {
-    console.log("--Setting show component to false--");
-    setComponent(false);
-  }, [selected_date, selected_district_id, selected_state]);
+  //Getting Available Sessions and their details
+  React.useEffect(() => {
+    if (selected_district_id && selected_date && valid) {
+      // console.log("Inside getSessions");
+      let date_string = selected_date.toLocaleString("en-GB").split(",")[0];
+      // console.log(
+      //   "Selected District: ",
+      //   selected_district_id,
+      //   " Selected date: ",
+      //   date_string
+      // );
+      let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${selected_district_id}&date=${date_string}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setSessionsList(data.sessions);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [selected_district_id, selected_date, valid]);
 
-  // Handle Change of State Names
-  function handleChange(changeObject) {
-    setState(changeObject);
-    setMessage("Now, please select a valid district");
-    console.log("Resetting district value");
-    setDistrict("");
+  // If any of the inputs change, table should be reset
+  useEffect(() => {
+    // console.log("--Setting show component to false--");
+    setComponent(false);
+  }, [selected_date, selected_district_id]);
+
+  // Handling change of State names
+  function handleChangeState(changeObject) {
+    setStateId(changeObject.value);
+    setMessage("Please select a valid district");
+    // console.log("Resetting district value");
     setDistrictId("");
     setSessionsList([]);
     setValidity(false);
   }
 
-  // Handling change of district names
+  // Handling change of District names
   function handleChangeDistrict(changeObject) {
-    setDistrict(changeObject.label);
     setDistrictId(changeObject.value);
     setValidity("true");
-    setMessage(
-      "Now choose a date and click Submit button to check availability!"
-    );
+    setMessage("Choose a date and click Submit button to check availability!");
   }
 
-  // Handling clicking Submit button
+  // Handling clicking of Submit button
   function buttonClick(e) {
     e.preventDefault();
     setComponent(true);
@@ -167,17 +140,15 @@ function App() {
 
   return (
     <div>
-      {/* <div> */}
       <form className="session-form col-lg-4 col-md-6 col-sm-12 col-xs-12">
         <div>
           <Select
             className="dropdown-select"
             styles={customStyles}
-            value={selected_state}
             placeholder="Choose a State"
             onChange={(selectedOption) => {
-              // console.log("State Chosen ", selectedOption);
-              handleChange(selectedOption);
+              console.log("State Chosen ", selectedOption.label);
+              handleChangeState(selectedOption);
             }}
             options={state_list}
             autoFocus={true}
@@ -185,25 +156,13 @@ function App() {
           />
         </div>
         <div>
-          {/* <Select
-            className="dropdown-select"
-            styles={customStyles}
-            placeholder="Choose a District"
-            onChange={(selectedOption) => {
-              console.log(selectedOption);
-              handleChangeDistrict(selectedOption);
-            }}
-            value={selected_district}
-            isSearchable
-            options={district_list}
-          /> */}
           <Select
             className="dropdown-select"
             styles={customStyles}
-            // value={selected_district}
+            required="true"
             placeholder="Choose a District"
             onChange={(selectedOption) => {
-              // console.log("Selected District ", selectedOption);
+              console.log("District chosen: ", selectedOption.label);
               handleChangeDistrict(selectedOption);
             }}
             options={district_list}
@@ -227,45 +186,37 @@ function App() {
       </form>
 
       <section className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-        {console.log(sessions_list)}
-        {
-          showComponent && sessions_list.length && valid ? (
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Hospital Name</th>
-                  <th>Address</th>
-                  <th>Available Capacity Dose 1</th>
-                  <th>Available Capacity Dose 2</th>
-                  <th>Vaccine Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* TODO: If no info show a message saying so */}
-                {sessions_list.map((hospital) => {
-                  return (
-                    <tr key={hospital.session_id}>
-                      <td>{hospital.name}</td>
-                      <td>{hospital.address}</td>
-                      <td>{hospital.available_capacity_dose1}</td>
-                      <td>{hospital.available_capacity_dose2}</td>
-                      <td>{hospital.vaccine}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          ) : showComponent && valid ? (
-            "Sorry! No sessions available😢"
-          ) : (
-            <div className="message"> {message} </div>
-          )
-          // showComponent ? (
-          //   "Sorry! No sessions available😢"
-          // ) : (
-          //   "Please input valid details to check availability"
-          // )
-        }
+        {showComponent && sessions_list.length && valid ? (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Hospital Name</th>
+                <th>Address</th>
+                <th>Available Capacity Dose 1</th>
+                <th>Available Capacity Dose 2</th>
+                <th>Vaccine Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* TODO: If no info show a message saying so */}
+              {sessions_list.map((hospital) => {
+                return (
+                  <tr key={hospital.session_id}>
+                    <td>{hospital.name}</td>
+                    <td>{hospital.address}</td>
+                    <td>{hospital.available_capacity_dose1}</td>
+                    <td>{hospital.available_capacity_dose2}</td>
+                    <td>{hospital.vaccine}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : showComponent && valid ? (
+          "Sorry! No sessions available😢"
+        ) : (
+          <div className="message"> {message} </div>
+        )}
       </section>
     </div>
   );
